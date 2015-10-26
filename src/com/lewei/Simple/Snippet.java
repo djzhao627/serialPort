37a6b45fb1b175168829f708dc27abb16edc2dd0
djzhao627 <894559192@qq.com>
2015-10-26 08:36:12 +0800

    备份串口程序

---
+++ src/com/lewei/Simple/Snippet.java
@@ -0,0 +1,20 @@
+package com.lewei.Simple;
+
+import java.util.Enumeration;
+
+import javax.comm.CommPortIdentifier;
+
+public class Snippet {
+	public static void main(String[] args) {
+		Enumeration<?> en = CommPortIdentifier.getPortIdentifiers();
+		CommPortIdentifier portId;
+		while (en.hasMoreElements()) {
+			portId = (CommPortIdentifier) en.nextElement();
+			// 如果端口类型是串口，则打印出其端口信息
+//			if (portId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+				System.out.println(portId.getName());
+//			}
+		}
+		System.out.println("finish");
+	}
+}
---
+++ src/com/lewei/message/test.java
@@ -0,0 +1,28 @@
+package com.lewei.message;
+
+
+public class test{
+
+
+	public static void main(String[] args) {
+        
+         ComputeSmsData sms = new ComputeSmsData();
+        SerialToGsm stg = new SerialToGsm("COM4");
+
+        String retStr = new String("");
+        String sss = new String();
+        String alarmNumber = new String("10086");
+
+        // check for messages
+        retStr = stg.checkSms();
+        if (retStr.indexOf("ERROR") == -1) {
+            System.out.println("Phone # of sender: " + stg.readSmsSender());
+            System.out.println("Recv'd SMS message: " + stg.readSms());
+        }
+       
+        // send a message
+      //  sss = stg.sendSms(alarmNumber,"jf");
+
+	}
+
+}
